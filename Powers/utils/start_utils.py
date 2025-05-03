e841804c1ee2340a977b00c84af5380a1ecdd8d3
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2025-05-03 15:33:59 +0530

    Update start_utils.py

--- Powers/utils/start_utils.py
+++ Powers/utils/start_utils.py
@@ -52,7 +52,7 @@ async def gen_start_kb(q: Message or CallbackQuery):
             [
                 (
                     "🗃️ Source Code",
-                    "https://github.com/Gojo-Bots/Gojo_Satoru",
+                    "https://t.me/AS_Jinwoo",
                     "url",
                 ),
                 (
@@ -64,7 +64,7 @@ async def gen_start_kb(q: Message or CallbackQuery):
             [
                 (
                     "❗️ Essential",
-                    "https://t.me/+PcVYvdzNt4E1YjM1",
+                    "https://t.me/ABS_Updates",
                     "url",
                 ),
                 (
